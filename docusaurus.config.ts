50cd78d96ba360247b19bf4cb65b6b83c7073917
Belsamor <49333265+Belsamor@users.noreply.github.com>
2026-09-08 10:52:32 +0200

    edited github link to my repo, added template link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -119,8 +119,12 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            }
+              href: repo_url,
+            },
+            {
+              label: 'Template',
+              href: 'https://github.com/Developer-Akademie-DevSecOpsKurs/dev-blog-template',
+            },
           ],
         },
       ],
